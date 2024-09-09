f2095902be1fea4b8051bc03dc4343504c4762ed
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-09-09 08:48:22 +0200

    works

--- src/test/java/dat/persistence/LocationDAOTest.java
+++ src/test/java/dat/persistence/LocationDAOTest.java
@@ -56,19 +56,38 @@ class LocationDAOTest {
         int ex = 3;
         int a = dao.getAll().size();
         assertEquals(a,ex);
-
-
     }
 
     @Test
     void create() {
+
+        Location l4 = Location.builder()
+                .latitude(4.1)
+                .longitude(4.1)
+                .address("hahahah").build();
+        dao.create(l4);
+        assertEquals(l4, dao.findById(l4.getId()));
+
     }
 
     @Test
     void update() {
+
+        Location updatedLocation = l1;
+        l1.setAddress("theAddressIsNowUpdated");
+        Location a = dao.update(updatedLocation);
+        String ex = "theAddressIsNowUpdated";
+        Assertions.assertEquals(ex, a.getAddress());
     }
 
     @Test
     void delete() {
+
+        int loca = dao.getAll().size();
+        dao.delete(l1);
+        int ex = loca - 1;
+        int a = dao.getAll().size();
+        assertEquals(ex,a);
+
     }
 }
